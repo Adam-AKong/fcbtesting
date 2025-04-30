05468533cf79da0a6088e69846ddc54cea7c8d6c
BrKap <259brian952@gmail.com>
2025-04-30 10:47:18 -0700

    V1 Alembic added more tables (need to finish columns and constraints)

--- src/AlembicModels.py
+++ src/AlembicModels.py
@@ -1,12 +1,66 @@
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, ForeignKey, Integer, String
 
 Base = declarative_base()
 
 class User(Base):
     __table__name__ = 'user'
+    
     id = Column(Integer, primary_key=True)
     name = Column(String, unique=True, nullable=False)
     # Figure out how to do Join Date
     
-# Create other Classes to import Tables into DB
+    __table_args__ = (
+        
+    ) 
+    
+class C_Review(Base):
+    __table__name__ = 'c_review'
+    
+    id = Column(Integer, primary_key=True)
+    user_id = Column (Integer, ForeignKey('user.id')) 
+    
+    __table_args__ = (
+        
+    ) 
+    
+class F_Review(Base):
+    __table__name__ = 'f_review'
+    
+    id = Column(Integer, primary_key=True)
+    user_id = Column (Integer, ForeignKey('user.id'))  
+    
+    __table_args__ = (
+        
+    ) 
+    
+class Character(Base):
+    __table__name__ = 'character'
+    
+    id = Column(Integer, primary_key=True)
+    user_id = Column (Integer, ForeignKey('user.id')) 
+    
+    __table_args__ = (
+        
+    ) 
+    
+    
+class Franchise(Base):
+    __table__name__ = 'franchise'
+    
+    id = Column(Integer, primary_key=True) 
+    
+    __table_args__ = (
+        
+    ) 
+    
+class Battle(Base):
+    __table__name__ = 'battle'
+    
+    id = Column(Integer, primary_key=True)      
+    user_id = Column (Integer, ForeignKey('user.id'))     
+    
+    __table_args__ = (
+        
+    )   
+    
