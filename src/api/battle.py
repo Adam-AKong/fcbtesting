f4d148211bbf20bec0d9d2a362855c68c13a8611
vic <vgrigory@calpoly.edu>
2025-05-05 10:36:10 -0700

    Battle Placeholder Code
    Should technically run now. No db calls

--- src/api/battle.py
+++ src/api/battle.py
@@ -1,6 +1,8 @@
+import sqlalchemy
 from fastapi import APIRouter, HTTPException, status
 from pydantic import BaseModel
 from src.api.models import Battle, Character
+# from src import database as db
 
 router = APIRouter(prefix="/battle", tags=["Battle"])
 
@@ -9,21 +11,105 @@ def get_battle_result(battle_id: int):
     """
     Get the result of a battle by its ID.
     """
-    pass
+    # More proper code
+    """
+    with db.engine.begin as connection:
+        battle = connection.execute(
+            sqlalchemy.text(
+                
+                SELECT *
+                FROM battle
+                WHERE id = :id
+                
+            ),
+            [{"id": battle_id}]
+        ).one()
+    
+    return Battle(
+        id=battle.id,
+        user_id=battle.user_id,
+        char1_id=battle.char1_id,
+        char2_id=battle.char2_id,
+        vote1=battle.vote1,
+        vote2=battle.vote2,
+        winner_id=battle.winner_id,
+    )
+    """
+    # Placeholder
+    return Battle(
+        id=1,
+        user_id=1,
+        char1_id=1,
+        char2_id=2,
+        vote1=0,
+        vote2=1,
+        winner_id=2
+    )
 
 @router.get("/get/{character_id}", response_model=list[Battle])
 def character_participation(character_id: int):
     """
     Get a list of battles a character has fought in.
     """
-    pass
+    # Proper
+    """
+    with db.engine.begin:
+        connection.execute(
+            sqlalchemy.text(
+                
+            )
+        )
+    """
+    # Placeholder
+    battles = [
+        Battle(
+            id=1,
+            user_id=1,
+            char1_id=1,
+            char2_id=2,
+            vote1=0,
+            vote2=1,
+            winner_id=2
+        ),
+        Battle(
+            id=2,
+            user_id=1,
+            char1_id=1,
+            char2_id=2,
+            vote1=1,
+            vote2=0,
+            winner_id=1
+        )
+    ]
+    return battles
 
-@router.get("/get/{user_id}", response_model="")
+@router.get("/get/{user_id}", response_model=list[Battle])
 def user_participation(user_id: int):
     """
     Get a list of battles a user has participated in.
     """
-    pass
+    # Placeholder
+    battles = [
+        Battle(
+            id=1,
+            user_id=1,
+            char1_id=1,
+            char2_id=2,
+            vote1=0,
+            vote2=1,
+            winner_id=2
+        ),
+        Battle(
+            id=2,
+            user_id=1,
+            char1_id=1,
+            char2_id=2,
+            vote1=1,
+            vote2=0,
+            winner_id=1
+        )
+    ]
+    return battles
 
 @router.get("/vote/{battle_id}/{character_id}", status_code=status.HTTP_204_NO_CONTENT)
 def battle_vote(battle_id: int, character_id: int):
